f236e0c4119e9b6ee3b2919c792d8c089c28bd6c
Evans N. Karago <148349906+evansnjagi@users.noreply.github.com>
2026-09-09 09:06:59 +0000

    1D arrays, vectorized operations, 2D arrays and indexing a matrix

---
+++ unit-2-numpy/code/intro_arrays.py
@@ -0,0 +1,50 @@
+# Import 
+import numpy as np
+import time
+
+a = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+print(a)
+print(type(a))
+
+# Create a large dataset
+size = 1_000_000
+python_list = list(range(size))
+numpy_array = np.array([python_list])
+
+# Method one, Python list sum
+start = time.time()
+total = 0.0
+
+for num in python_list:
+    total += num
+python_time = time.time() - start
+
+# Method two, NumPy sum
+start = time.time()
+numpy_total = np.sum(numpy_array)
+numpy_time = time.time() - start
+
+print(f"Python loop time: {python_time:.5f} seconds.")
+print(f"NumPy time: {numpy_time:.5f} seconds.")
+print(f"Numpy is {python_time / numpy_time} x faster.")
+
+# Numpy array operations
+b = np.arange(0, 20, 2)
+print(b)
+print(b.shape)
+print(b[2: 5])
+
+# Vectorized operations
+temp_celsius = np.array([0, 10, 20, 30, 40])
+temp_farenheit = temp_celsius * 9 / 5 + 32
+
+print(temp_farenheit)
+
+
+# Matrix
+matrix = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
+print(f"Matrix X = \n{matrix}")
+print(matrix.shape)
+print(matrix[1, 2])
+print(matrix[0,:])
+print(matrix[:, 2])
